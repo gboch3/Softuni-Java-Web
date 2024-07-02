975608b6ed99ac18832688807d65fd985f519299
gbch3v <gbochev@mail.bg>
2024-07-02 17:36:44 +0300

    Mobilele REST API implementation

---
+++ L01_REST-API-and-REST-with-Spring/mobilele/src/main/java/bg/softuni/mobilele/service/impl/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package bg.softuni.mobilele.service.impl;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
