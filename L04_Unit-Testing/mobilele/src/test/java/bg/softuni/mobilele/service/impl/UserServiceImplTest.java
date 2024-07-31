82a24235c9887f1f4110728f21b8807937af2b2b
gbch3v <gbochev@mail.bg>
2024-08-01 00:04:09 +0300

    Unit Testing Captor Implementation

--- L04_Unit-Testing/mobilele/src/test/java/bg/softuni/mobilele/service/impl/UserServiceImplTest.java
+++ L04_Unit-Testing/mobilele/src/test/java/bg/softuni/mobilele/service/impl/UserServiceImplTest.java
@@ -1,19 +1,29 @@
 package bg.softuni.mobilele.service.impl;
 
 import bg.softuni.mobilele.model.dto.UserRegistrationDTO;
+import bg.softuni.mobilele.model.entity.UserEntity;
 import bg.softuni.mobilele.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
 
     private UserServiceImpl toTest;
+
+    @Captor
+    private ArgumentCaptor<UserEntity> userEntityCaptor;
 
     @Mock
     private UserRepository mockUserRepository;
@@ -40,9 +50,21 @@ class UserServiceImplTest {
                         .setPassword("topsecret")
                         .setEmail("anna@example.com");
 
+        when(mockPasswordEncoder.encode(userRegistrationDTO.getPassword()))
+                .thenReturn(userRegistrationDTO.getPassword() + userRegistrationDTO.getPassword());
+
         // ACT
         toTest.registerUser(userRegistrationDTO);
 
-        // TODO
+        // Assert
+        verify(mockUserRepository).save(userEntityCaptor.capture());
+
+        UserEntity actualSavedEntity = userEntityCaptor.getValue();
+
+        Assertions.assertEquals(userRegistrationDTO.getFirstName(), actualSavedEntity.getFirstName());
+        Assertions.assertEquals(userRegistrationDTO.getLastName(), actualSavedEntity.getLastName());
+        Assertions.assertEquals(userRegistrationDTO.getPassword() + userRegistrationDTO.getPassword(),
+                actualSavedEntity.getPassword());
+        Assertions.assertEquals(userRegistrationDTO.getEmail(), actualSavedEntity.getEmail());
     }
 }
